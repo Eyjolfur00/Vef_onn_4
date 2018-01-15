1b5b3d98141e311c278855420c48dd090ec2e2d5
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2018-01-15 11:24:32 +0000

    Update Verkefni1.py

--- verk1/Verkefni1.py
+++ verk1/Verkefni1.py
@@ -1,1 +1,22 @@
+@route('/')
+def vid():
+    return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Mass effect</title>
+                   <meta charset="utf-8">
+                   
 
+               </head>
+               <body>
+                   <ul>
+                       <li><a href="http://tsuts.tskoli.is/2t/0711992829/vsh2/verkefni6">Mass effect</a></li>
+                       <li><a href="http://tsuts.tskoli.is/2t/0711992829/Verkefni1/verkefni1.html">Biography</a></li>
+                       <li><a href="/pic">Pictures</a></li>
+                   </ul>
+
+                   <h1>Mass effect</h1>
+                    <img src="static/mass-effect-3.jpg" alt="Mass effect">
+               </body>
+           </html>
