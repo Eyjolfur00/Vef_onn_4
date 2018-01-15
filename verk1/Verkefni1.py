17c67a66c2850ff03f31e9d7849b6254f642e99d
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2018-01-15 11:42:51 +0000

    Update Verkefni1.py

--- verk1/Verkefni1.py
+++ verk1/Verkefni1.py
@@ -1,22 +1,9 @@
-@route('/')
-def vid():
-    return """
-    <!DOCTYPE html>
-           <html>
-               <head>
-                   <title>Mass effect</title>
-                   <meta charset="utf-8">
-                   
+from bottle import *
+import os
 
-               </head>
-               <body>
-                   <ul>
-                       <li><a href="http://tsuts.tskoli.is/2t/0711992829/vsh2/verkefni6">Mass effect</a></li>
-                       <li><a href="http://tsuts.tskoli.is/2t/0711992829/Verkefni1/verkefni1.html">Biography</a></li>
-                       <li><a href="/pic">Pictures</a></li>
-                   </ul>
+@route("/")
+def index():
+    return"hallo heimur"
 
-                   <h1>Mass effect</h1>
-                    <img src="static/mass-effect-3.jpg" alt="Mass effect">
-               </body>
-           </html>
+run(host="0.0.0.0", port=os.environ.get("PORT"))
+#run(host="localhost", port8080)
