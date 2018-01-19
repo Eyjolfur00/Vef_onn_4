46a190a5f0b5e5317c27357a5163801b73e72134
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2018-01-19 14:25:39 +0000

    Update Verkefni1.py

--- Verkefni1.py
+++ Verkefni1.py
@@ -1,9 +1,99 @@
 from bottle import *
 import os
 
-@route("/")
+@route('/')
 def index():
-    return"hallo heimur"
+    return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Verkefni1</title>
+                   <meta charset="utf-8">
+               </head>
+               <body>
+                   <h1>Sidur</h1>
+                   <ul>
+                       <li><a href="/1">forsida</a></li>
+                       <li><a href="/2">totierkoolkid</a></li>
+                       <li><a href="/3">supremehere</a></li>
+                   </ul>
 
-run(host="0.0.0.0", port=os.environ.get("PORT"))
-#run(host="localhost", port8080)
+                </body>
+           </html>
+            """
+@route('/<nr>')
+def index(nr):
+    if nr =="0":
+        return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Verkefni1</title>
+                   <meta charset="utf-8">
+               </head>
+               <body>
+                   <h1>Sidur</h1>
+                   <ul>
+                       <li><a href="/1">forsida</a></li>
+                       <li><a href="/2">totierkoolkid</a></li>
+                       <li><a href="/3">supremehere</a></li>
+                   </ul>
+
+                </body>
+           </html>
+            """
+    
+    if nr=='1':
+        return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Verkefni1</title>
+                   <meta charset="utf-8">
+               </head>
+               <body>
+                   <h1>Forsíða</h1>
+                   <ul>
+                       <li><a href="/0">Sidur</a></li>
+                       <li><a href="/2">totierkoolkid</a></li>
+                       <li><a href="/3">supremehere</a></li>
+                   </ul>
+            """
+
+    if nr=='2':
+        return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Verkefni1</title>
+                   <meta charset="utf-8">
+               </head>
+               <body>
+                   <h1>TotierKOOLkid </h1>
+                   <ul>
+                       <li><a href="/0">Sida</a></li>
+                       <li><a href="/1">Forsíða</a></li>
+                       <li><a href="/3">supremehere</a></li>
+                   </ul>
+            """
+    if nr=='3':
+        return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Verkefni1</title>
+                   <meta charset="utf-8">
+               </head>
+               <body>
+                   <h1>Supremehere</h1>
+                   <ul>
+                       <li><a href="/0">Sída</a></li>
+                       <li><a href="/1">forsida</a></li>
+                       <li><a href="/2">totierkoolkid</a></li>
+                   </ul>
+            """
+
+
+run(host="localhost", port=8080, debug = True)
+
+
